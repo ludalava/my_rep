d2c8a20abc5198c095b010f8ab88ea5825b81d90
unknown <sunnyhawaiibymouse@gmail.com>
2023-09-21 15:52:46 +0300

    Homework 1 Lesson

---
+++ Homework1.py
@@ -0,0 +1,34 @@
+a=7
+b="Hello"
+c=13.4
+print (a,b,c)
+print(id(a))
+print(id(b))
+print(id(c))
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+a="Hello"
+print(id(a))
+
+A=6
+B=6
+C=6
+print(A,B,C)
+
+print(id(A))
+print(id(B))
+print(id(C))
+
+print("Введите a")
+print("Введите b")
+print("Введите c")
+
+
+a=input()
+b=input()
+c=input()
+print(id(a),id(b),id(c))
+print(type(a),type(b),type(c))
